af20129091ef3d7b2e33d8e1e8932d826ae3a6f8
冯楷迪 <694837707@qq.com>
2024-08-04 20:47:24 +0800

    feat：添加脚本

---
+++ code-review-sdk/src/main/java/com/DXG/AiCodeReview.java
@@ -0,0 +1,13 @@
+package com.DXG;
+
+/**
+ * @author: DXG
+ * @description: TODO
+ * @datetime: 2024/8/4 20:41
+ **/
+
+public class AiCodeReview {
+    public static void main(String[] args) {
+        System.out.println("测试执行");
+    }
+}
